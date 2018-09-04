4325ad82a5ed0307b730a64015dbccbce392a5ae
cofitost <zxcv091922@gmail.com>
2018-09-04 17:46:42 +0800

    delete的checkout data 未完成

--- routes/pointData.js
+++ routes/pointData.js
@@ -9,10 +9,11 @@ router.get('/', function(req, res, next) {
 router.post('/post', function(req, res, next) {
     var dataModel = new model();
 
-    var today = new Date();
+    var today = new Date;
+    var millisecond = today.getMilliseconds();
 
     dataModel.station = req.body.station;
-    dataModel.post_at = today;
+    dataModel.post_at = today+'毫秒數:'+millisecond;
     dataModel.times = req.body.times;
     dataModel.data_1 = req.body.data_1;
 
@@ -30,6 +31,23 @@ router.get('/get', function(req, res, nest) {
             res.send(err);
 
         res.json(data);
+    });
+});
+
+router.delete('/delete/:times/:station', function(req, res, next) {
+
+    model.find({'times': req.params.times,'station': req.params.station}, function (err, data) {
+        console.log(data.length+'');
+        if (data.length == 0){
+            res.send('data not exists',null);
+        }else {
+            model.deleteMany({'times': req.params.times,'station': req.params.station}, function(err) {
+                if(err)
+                    res.send(err);
+        
+                res.send('Done!');
+            });
+        }
     });
 });
 
